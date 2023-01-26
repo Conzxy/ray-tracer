67f8a57a4fd33b5dd6ca9e0a97d065d96367327a
Conzxy <1967933844@qq.com>
2023-01-26 16:48:28 +0800

    引入了更多场景

--- src/main.cc
+++ src/main.cc
@@ -1,21 +1,32 @@
 #include <cstdio>
 
+#include <mutex>
 #include <string_view>
 #include <thread>
 
+#include "accelerate/bvh_node.hh"
 #include "gm/util.hh"
 #include "img/color.hh"
 #include "img/tga_image.hh"
 #include "material/dielectric.hh"
+#include "material/diffuse_light.hh"
 #include "material/lambertian.hh"
 #include "material/matal.hh"
 #include "rt/camera.hh"
 #include "rt/hit_record.hh"
 #include "rt/ray.hh"
+#include "shape/box.hh"
+#include "shape/rect.hh"
+#include "shape/rotate.hh"
 #include "shape/shape_list.hh"
 #include "shape/sphere.hh"
-#include "util/str_cvt.hh"
+#include "texture/checker_texture.hh"
+#include "texture/image_texture.hh"
+#include "texture/solid_texture.hh"
+#include "util/atomic_counter.h"
+#include "util/progress_bar.hh"
 #include "util/random.hh"
+#include "util/str_cvt.hh"
 
 #define MAX_DEPTH 50
 
@@ -27,9 +38,9 @@ using namespace std;
 
 namespace ktm = std::chrono;
 
-using MaterialVector = std::vector<std::unique_ptr<Material>>;
+Vec3F ray_color(Ray const &ray, rt::Color const &background, Shape const &shape,
+                int max_depth);
 
-Vec3F ray_color(Ray const &ray, Shape const &shape, int max_depth);
 img::Color compute_color(Vec3F const &c, int sample_per_pixel,
                          double gamma_exp);
 
@@ -39,14 +50,16 @@ struct Option {
   int thread_num = 8;
   int gamma = 2;
   int image_height = 400;
-
+  int scene_id = -1;
   void DebugPrint() const;
 };
 
 bool parse_options(int argc, char *argv[], Option *option);
-void setup_scene(ShapeList &world,
-                 std::vector<std::unique_ptr<Material>> &materials);
-void setup_random_scene(ShapeList &world, MaterialVector &mats);
+void setup_scene(ShapeList &world);
+void setup_random_scene(ShapeList &world);
+void setup_light_scene(ShapeList &world);
+void setup_image_scene(ShapeList &world);
+void setup_cornellbox(ShapeList &world);
 
 int main(int argc, char *argv[])
 {
@@ -60,23 +73,61 @@ int main(int argc, char *argv[])
 
   // Setup camera
   double aspect_ratio = 16. / 9.;
-  Point3F lookfrom(-2., 2., 0.);
-  // Point3F lookfrom(0, 0, 0);
-  Point3F lookat(0., 0., -1.);
-  auto focus_dist = (lookat-lookfrom).length();
-  Camera camera(lookfrom, lookat, aspect_ratio, 50, focus_dist);
-  camera.set_aperture(5.0);
-  camera.DebugPrint();
-
-  // Setup image
-  int image_width = aspect_ratio * option.image_height;
-  TgaImage image(image_width, option.image_height);
 
   // Setup Scene
   ShapeList world;
-  std::vector<std::unique_ptr<Material>> materials;  
-  setup_scene(world, materials);
-  // setup_random_scene(world, materials);
+
+  rt::Color background(0, 0, 0);
+  Point3F lookfrom(0, 0, 0);
+  Point3F lookat(0, 0, -1);
+  double fov = 90;
+
+  switch (option.scene_id) {
+    case 0: {
+      setup_scene(world);
+    } break;
+    case 1: {
+      background = rt::Color(0.8, 0.4, 0.3);
+      lookfrom = Point3F(13., 2., 3.);
+      // Point3F lookfrom(0, 0, 0);
+      lookat = Point3F(0, 0, 0);
+      // auto focus_dist = (lookat-lookfrom).length();
+      fov = 30;
+      setup_random_scene(world);
+    } break;
+
+    case 2: {
+      background = rt::Color(0, 0, 0);
+      lookfrom = Point3F(26, 3, 6);
+      lookat = Point3F(0, 2, 0);
+      fov = 20;
+      setup_light_scene(world);
+    } break;
+
+    case 3: {
+      background = rt::Color(1, 1, 1);
+      lookfrom = Point3F(13, 2, 3);
+      lookat = Point3F(0, 0, 0);
+      fov = 20;
+      setup_image_scene(world);
+    } break;
+
+    default:
+    case 4: {
+      // background = rt::Color(1, 1, 1);
+      lookfrom = Point3F(0, 278, 800);
+      lookat = Point3F(0, 278, 0);
+      fov = 40;
+      setup_cornellbox(world);
+    } break;
+  }
+
+  Camera camera(lookfrom, lookat, aspect_ratio, fov, 1);
+  camera.set_aperture(0.0);
+  camera.DebugPrint();
+  // Setup image
+  int image_width = aspect_ratio * option.image_height;
+  TgaImage image(image_width, option.image_height);
 
   std::vector<std::thread> thrs;
   thrs.reserve(option.thread_num);
@@ -86,6 +137,10 @@ int main(int argc, char *argv[])
   printf("init start = %d\n", start);
   printf("slice = %d\n", slice);
 
+  const size_t total_sample =
+      image.height() * image.width() * option.sample_per_pixel;
+  AtomicCounter64 current_complete_sample(0);
+
   auto start_of_render = ktm::steady_clock::now();
 
   for (int ti = 0; ti < option.thread_num; ++ti) {
@@ -94,9 +149,10 @@ int main(int argc, char *argv[])
       end = 0;
     }
 
-    printf("slice [%d, %d]\n", start, end);
-    thrs.emplace_back(std::thread(
-        [start, end, &option, gamma_exp, &world, &camera, &image]() {
+    // printf("slice [%d, %d]\n", start, end);
+    thrs.emplace_back(
+        std::thread([start, end, &option, gamma_exp, &background, &world,
+                     &camera, &image, &current_complete_sample]() {
           for (int j = start; j >= end; --j) {
             for (int i = 0; i < image.width(); ++i) {
               // propertion
@@ -107,7 +163,8 @@ int main(int argc, char *argv[])
                 auto v = double(j + offset) / (image.height() - 1);
 
                 auto ray = camera.ray(u, v);
-                color_prop += ray_color(ray, world, MAX_DEPTH);
+                color_prop += ray_color(ray, background, world, MAX_DEPTH);
+                current_complete_sample++;
               }
               auto color =
                   compute_color(color_prop, option.sample_per_pixel, gamma_exp);
@@ -119,15 +176,23 @@ int main(int argc, char *argv[])
     start -= slice + 1;
   }
 
+  while (1) {
+    auto current_value = current_complete_sample.GetValue();
+    if (current_value >= total_sample) break;
+    update_progress_bar('#', current_value / double(total_sample) * 100);
+    std::this_thread::sleep_for(1s);
+  }
+  update_progress_bar('#', 100);
+
   for (auto &thr : thrs) {
     thr.join();
   }
 
   auto end_of_render = ktm::steady_clock::now();
   ktm::duration<double> cost_time_of_render = end_of_render - start_of_render;
-  printf("The consume time of render is %.3lfsec\n",
+  printf("\nThe consume time of render is %.3lf sec\n",
          cost_time_of_render.count());
-
+  fflush(stdout);
   image.WriteTo(option.path);
 
   return EXIT_SUCCESS;
@@ -140,6 +205,12 @@ int main(int argc, char *argv[])
   "[--gamma/-g integer]"                                                       \
   "[--height/-h integer]\n",                                                   \
       argv[0]
+
+inline bool check_option(std::string_view opt, char const *lopt,
+                         char const *sopt) noexcept
+{
+  return opt == lopt || opt == sopt;
+}
 
 bool parse_options(int argc, char *argv[], Option *option)
 {
@@ -196,6 +267,14 @@ bool parse_options(int argc, char *argv[], Option *option)
         return false;
       }
       option->gamma = *ret;
+
+    } else if (check_option(opt, "--scene", "-s")) {
+      auto ret = util::str2int(arg);
+      if (!ret) {
+        fprintf(stderr, "The argument of --scene/-s is invalid\n");
+        return false;
+      }
+      option->scene_id = *ret;
     } else {
       fprintf(stderr, "Unknown option: %s\n", *argv);
       return false;
@@ -208,26 +287,33 @@ bool parse_options(int argc, char *argv[], Option *option)
   return true;
 }
 
-Vec3F ray_color(Ray const &ray, Shape const &shape, int max_depth)
+Vec3F ray_color(Ray const &ray, rt::Color const &background, Shape const &world,
+                int max_depth)
 {
   if (max_depth <= 0) return {0, 0, 0};
 
   HitRecord record;
-  if (shape.hit(ray, 0.001, inf, record)) {
+  if (world.hit(ray, 0.001, inf, record)) {
     rt::Color attenuation;
     Ray out_ray;
+    auto emitted = record.material->emitted(record.u, record.v, record.p);
     if (record.material->scatter(ray, record, &attenuation, &out_ray)) {
-      return attenuation * ray_color(out_ray, shape, max_depth - 1);
+      return attenuation *
+                 ray_color(out_ray, background, world, max_depth - 1) +
+             emitted;
     } else {
-      return {0, 0, 0};
+      return emitted;
     }
   }
 
+#if 0
   auto unit_direction = ray.direction().normalize();
   // y可能是负值
   // [-1, 1] ==> [0, 1]
   auto t = 0.5 * (unit_direction.y + 1.0);
   return lerp(Vec3F(1.0, 1.0, 1.0), Vec3F(0.5, 0.7, 1.0), t);
+#endif
+  return background;
 }
 
 img::Color compute_color(Vec3F const &c, int sample_per_pixel, double gamma_exp)
@@ -252,66 +338,108 @@ void Option::DebugPrint() const
   printf("gamma = %d\n", gamma);
 }
 
-void setup_scene(ShapeList &world,
-                 std::vector<std::unique_ptr<Material>> &materials)
+void setup_scene(ShapeList &world)
 {
-  // Setup material
-  auto material_ground = std::make_unique<Lambertian>(rt::Color(0.8, 0.8, 0));
-  auto material_center = std::make_unique<Lambertian>(rt::Color(0.7, 0.3, 0.3));
+  // setup textures
+  auto checker_texture =
+      std::make_shared<CheckerTexture>(rt::Color(0, 0, 0), rt::Color(1, 1, 1));
+  // Setup materia1, 1, 1l
+  auto material_ground = std::make_shared<Lambertian>(checker_texture);
+  auto material_center = std::make_shared<Lambertian>(rt::Color(0.7, 0.3, 0.3));
   // auto material_center = std::make_unique<Dielectric>(1.5);
   // auto material_left = std::make_unique<Matal>(rt::Color(0.8, 0.8, 0.8),
   // 0.0);
-  auto material_left = std::make_unique<Dielectric>(1.5);
-  auto material_right = std::make_unique<Matal>(rt::Color(0.8, 0.6, 0.2), 0.0);
+  auto material_left = std::make_shared<Dielectric>(1.5);
+  auto material_right = std::make_shared<Matal>(rt::Color(0.8, 0.6, 0.2), 0.0);
 
   // Setup shapes
+  world.add(std::make_shared<Sphere>(Point3F(0, 0, -1), 0.5, material_center));
   world.add(
-      std::make_shared<Sphere>(Point3F(0, 0, -1), 0.5, material_center.get()));
-  world.add(std::make_shared<Sphere>(Point3F(0, -100.5, -1), 100,
-                                     material_ground.get()));
-  world.add(
-      std::make_shared<Sphere>(Point3F(-1.0, 0, -1), 0.5, material_left.get()));
-  world.add(
-      std::make_shared<Sphere>(Point3F(1.0, 0, -1), 0.5, material_right.get()));
-
-  materials.push_back(std::move(material_ground));
-  materials.push_back(std::move(material_center));
-  materials.push_back(std::move(material_left));
-  materials.push_back(std::move(material_right));
+      std::make_shared<Sphere>(Point3F(0, -100.5, -1), 100, material_ground));
+  world.add(std::make_shared<Sphere>(Point3F(-1.0, 0, -1), 0.5, material_left));
+  world.add(std::make_shared<Sphere>(Point3F(1.0, 0, -1), 0.5, material_right));
 }
 
-void setup_random_scene(ShapeList &world, MaterialVector &mats)
+void setup_random_scene(ShapeList &world)
 {
-  auto material_ground = make_unique<Lambertian>(rt::Color(0.5, 0.8, 0.5));
-  world.add(make_shared<Sphere>(Point3F(0, -1000, 0), 1000, material_ground.get()));
-  mats.push_back(std::move(material_ground));
+  auto material_ground = make_shared<Lambertian>(rt::Color(0.5, 0.8, 0.5));
+  world.add(make_shared<Sphere>(Point3F(0, -1000, 0), 1000, material_ground));
 
-  for (int a = -11; a < 11; ++a) {
+  for (int a = -11; a < 110; ++a) {
     for (int b = -11; b < 11; ++b) {
       auto choose_mat = random_double();
-      Point3F center(a + 0.9 * random_double(), 0.2, b + 0.9*random_double()); 
-      
-      if ((center - Point3F(4, 0.2, 0)).length() > 0.9) {
-          if (choose_mat < 0.8) {
-            auto albedo = rt::Color::random() * rt::Color::random();
-            mats.push_back(std::make_unique<Lambertian>(albedo));
-          } else if (choose_mat < 0.95) {
-            auto albedo = rt::Color::random(0.5, 1);
-            auto fuzz = random_double(0, 0.5);
-            mats.push_back(std::make_unique<Matal>(albedo, fuzz));
-          } else {
-            mats.push_back(std::make_unique<Dielectric>(1.5));
-          }
+      Point3F center(a + 0.9 * random_double(), 0.2, b + 0.9 * random_double());
 
-          world.add(make_shared<Sphere>(center, 0.2, mats.back().get()));
+      if ((center - Point3F(4, 0.2, 0)).length() > 0.9) {
+        MaterialSPtr material;
+        if (choose_mat < 0.8) {
+          auto albedo = rt::Color::random() * rt::Color::random();
+          material = std::make_shared<Lambertian>(albedo);
+        } else if (choose_mat < 0.95) {
+          auto albedo = rt::Color::random(0.5, 1);
+          auto fuzz = random_double(0, 0.5);
+          material = std::make_shared<Matal>(albedo, fuzz);
+        } else {
+          material = std::make_shared<Dielectric>(1.5);
+        }
+
+        world.add(make_shared<Sphere>(center, 0.2, std::move(material)));
       }
     }
   }
 
-  mats.push_back(make_unique<Dielectric>(1.5));
-  world.add(make_shared<Sphere>(Point3F(0,1,0), 1.0, mats.back().get()));
-  mats.push_back(make_unique<Lambertian>(rt::Color(0.4, 0.2, 0.1)));
-  world.add(make_shared<Sphere>(Point3F(-4, 1, 0), 1.0, mats.back().get()));
-  mats.push_back(make_unique<Matal>(rt::Color(0.7, 0.6, 0.5), 0.0));
-  world.add(make_shared<Sphere>(Point3F(4, 1, 0), 1.0, mats.back().get()));
+  world.add(
+      make_shared<Sphere>(Point3F(0, 1, 0), 1.0, make_shared<Dielectric>(1.5)));
+  world.add(
+      make_shared<Sphere>(Point3F(-4, 1, 0), 1.0,
+                          make_shared<Lambertian>(rt::Color(0.4, 0.2, 0.1))));
+  world.add(
+      make_shared<Sphere>(Point3F(4, 1, 0), 1.0,
+                          make_shared<Matal>(rt::Color(0.7, 0.6, 0.5), 0.0)));
+}
+
+void setup_light_scene(ShapeList &world)
+{
+  auto light_texture = make_shared<SolidTexture>(rt::Color(16, 4, 4));
+  auto diffuse_light_material = make_shared<DiffuseLight>(light_texture);
+  auto rect_light = make_shared<XyRect>(3, 5, 1, 3, -2, diffuse_light_material);
+  world.add(std::move(rect_light));
+  world.add(make_shared<Sphere>(Point3F(0, 7, 0), 1, diffuse_light_material));
+  world.add(make_shared<Sphere>(
+      Point3F(0, 2, 0), 2, make_shared<Lambertian>(rt::Color(0.4, 0.6, 0.9))));
+  world.add(
+      make_shared<Sphere>(Point3F(0, -1000, 0), 1000,
+                          make_shared<Lambertian>(rt::Color(0.3, 0.2, 0.8))));
+}
+
+void setup_image_scene(ShapeList &world)
+{
+  auto earth_texture = make_shared<ImageTexture>("img/earthmap.jpg");
+  std::cout << *earth_texture << "\n";
+  world.add(make_shared<Sphere>(Point3F(0, 0, 0), 2,
+                                make_shared<Lambertian>(earth_texture)));
+}
+
+void setup_cornellbox(ShapeList &world)
+{
+  auto red = make_shared<Lambertian>(rt::Color(.65, .05, .05));
+  auto green = make_shared<Lambertian>(rt::Color(.12, .65, .45));
+  auto white = make_shared<Lambertian>(rt::Color(.75, .75, .75));
+  auto light = make_shared<DiffuseLight>(rt::Color(16, 16, 16));
+
+  world.add(make_shared<YzRect>(0, 556, -556, 0, -278, green));   // left
+  world.add(make_shared<YzRect>(0, 556, -556, 0, 278, red));      // right
+  world.add(make_shared<XyRect>(-278, 278, 0, 556, -556, white)); // front face
+  world.add(make_shared<XzRect>(-278, 278, -556, 0, 0, white));   // bottom
+  world.add(make_shared<XzRect>(-278, 278, -556, 0, 556, white)); // top
+  world.add(XzRect::create_based_mid(0, 200, -278, 200, 554, light));
+  ShapeSPtr box1 =
+      make_shared<Box>(Point3F(278 - 295, 0, -230),
+                       Point3F(278 - 130, 165, -65), white); // right
+  ShapeSPtr box2 = make_shared<Box>(Point3F(278 - 430, 0, -460),
+                                    Point3F(278 - 265, 330, -295), white); // left
+  // box1 = make_shared<Rotate>(std::move(box1), Degree{.y = -18});
+  // box2 = make_shared<Rotate>(std::move(box2), Degree{.y = 15});
+  world.add(box1);
+  world.add(box2);
 }
