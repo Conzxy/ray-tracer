15850f2ac4ea13bdb8a51cbd425f51ae6c0ec442
Conzxy <1967933844@qq.com>
2023-01-30 15:57:48 +0800

    make rect be a unilight source

--- src/shape/flip_face.hh
+++ src/shape/flip_face.hh
@@ -8,16 +8,28 @@ namespace rt {
 class FlipFace : public Shape {
  public:
   explicit FlipFace(ShapeSPtr &&shape)
-   : shape_(std::move(shape))
+    : shape_(std::move(shape))
   {
   }
 
-  virtual bool hit(Ray const &ray, double tmin, double tmax, HitRecord &rec) const;
-  virtual bool get_bounding_box(Aabb &bbox) const;
+  virtual double pdf_value(Point3F const &origin,
+                           Vec3F const &direction) const override
+  {
+    return shape_->pdf_value(origin, direction);
+  }
+  virtual Vec3F random_direction(Point3F const &origin) const override
+  {
+    return shape_->random_direction(origin);
+  }
+
+  virtual bool hit(Ray const &ray, double tmin, double tmax,
+                   HitRecord &rec) const override;
+  virtual bool get_bounding_box(Aabb &bbox) const override;
+
  private:
   ShapeSPtr shape_;
 };
 
-}
+} // namespace rt
 
 #endif
