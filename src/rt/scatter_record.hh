322e64cb2301aa1c39e66dab22fe426df2a4aafb
Conzxy <1967933844@qq.com>
2023-01-30 15:58:07 +0800

    used for rendering equation(diffuse) and specular material

--- src/rt/scatter_record.hh
+++ src/rt/scatter_record.hh
@@ -1,10 +1,17 @@
 #ifndef RT_SCATTER_RECORD_HH__
 #define RT_SCATTER_RECORD_HH__
 
+#include "../sample/pdf.hh"
+#include "../rt/ray.hh"
+#include "../rt/color.hh"
+
 namespace rt {
 
 struct ScatterRecord {
-  double pdf;
+  Ray specular_ray;
+  bool is_specular = true;
+  Color attenuation;
+  PdfSPtr pdf = nullptr;
 };
 
 } // namespace rt
