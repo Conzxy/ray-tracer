920e655bed605c6c3172695dc19edaaa0068c56d
Conzxy <1967933844@qq.com>
2023-01-30 15:55:59 +0800

    add: sampling interfaces

--- src/shape/shape.hh
+++ src/shape/shape.hh
@@ -15,6 +15,27 @@ class Shape
  public:
   virtual bool hit(Ray const &ray, double tmin, double tmax, HitRecord &record) const = 0;
   virtual bool get_bounding_box(Aabb &output_box) const = 0;
+  
+  /**
+   * \param origin The origin of the scatter ray
+   * \param direction The direction of the scatter ray
+   */
+  virtual double pdf_value(gm::Point3F const &origin, gm::Vec3F const &direction) const
+  {
+    // FIXME Pure virtual function
+    return 0;
+  }
+
+  /**
+   * \param origin The origin of the scatter ray(i.e. the intersected point)
+   */
+  virtual gm::Vec3F random_direction(gm::Point3F const &origin) const
+  {
+    // FIXME Pure virtual function
+    std::cout << "FIXME random_direction\n";
+    return { 0, 0, 0 };
+  }
+
   Aabb get_bounding_box() const;
 };
 
