2ee961d348cc9f84e1e7a9c66836c74122eb61e2
Conzxy <1967933844@qq.com>
2023-02-03 22:34:27 +0800

    代替UNIX的endian.h（仅限小端Windows）

---
+++ src/util/endian.h
@@ -0,0 +1,29 @@
+#ifndef CONZXY_ENDIAN_HH__
+#define CONZXY_ENDIAN_HH__
+
+#if defined(__unix__)
+
+# include <endian.h>
+
+#else defined(_WIN32)
+
+#include <winsock2.h>
+
+#define htobe16(x) htons(x)
+#define htole16(x) (x)
+#define be16toh(x) ntohs(x)
+#define le16toh(x) (x)
+
+#define htobe32(x) htonl(x)
+#define htole32(x) (x)
+#define be32toh(x) ntohl(x)
+#define le32toh(x) (x)
+
+#define htobe64(x) htonll(x)
+#define htole64(x) (x)
+#define be64toh(x) ntohll(x)
+#define le64toh(x) (x)
+
+#endif
+
+#endif
