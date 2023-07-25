d04158c97cef5df8dc161dc786690cd0844ad684
Conzxy <1967933844@qq.com>
2023-07-25 23:44:37 +0800

    fix(file): Add `<stdint.h>`
    `uint8_t` need it.

--- src/util/file.hh
+++ src/util/file.hh
@@ -1,29 +1,30 @@
-#ifndef KURO_UTIL_FILE_H__ 
-#define KURO_UTIL_FILE_H__ 
+#ifndef KURO_UTIL_FILE_H__
+#define KURO_UTIL_FILE_H__
 
-#include <stdio.h>
 #include <stddef.h>
-#include <string>
 #include <stdexcept>
+#include <stdint.h>
+#include <stdio.h>
+#include <string>
 
 #include "../util/noncopyable.hh"
 
 namespace util {
 
 struct FileException : std::runtime_error {
-  explicit FileException(char const* msg)
+  explicit FileException(char const *msg)
     : std::runtime_error(msg)
   {
   }
 
-  explicit FileException(std::string const& msg) 
+  explicit FileException(std::string const &msg)
     : std::runtime_error(msg)
   {
   }
 };
 
 /**
- * Wrap Standard C library File API 
+ * Wrap Standard C library File API
  * into RAII style
  */
 class File : kanon::noncopyable {
@@ -41,7 +42,7 @@ class File : kanon::noncopyable {
   };
 
   File() = default;
-  
+
   /**
    * \brief Open file in specific mode
    * \param filename The path of file
@@ -49,9 +50,9 @@ class File : kanon::noncopyable {
    *
    * \exception FileException
    */
-  File(char const* filename, int mode);
-  File(std::string const& filename, int mode);
-  
+  File(char const *filename, int mode);
+  File(std::string const &filename, int mode);
+
   /**
    * Like the constructor but don't throw exception
    * instead return false to indicates failure
@@ -59,26 +60,26 @@ class File : kanon::noncopyable {
    * \return
    *  true -- Success
    */
-  bool Open(std::string const& filename, int mode)
+  bool Open(std::string const &filename, int mode)
   {
     return Open(filename.data(), mode);
   }
 
-  bool Open(char const* filename, int mode);
+  bool Open(char const *filename, int mode);
 
   ~File() noexcept;
-  
+
   /**
    * \brief Read file content in len size
    * \param buf The buffer store contents
    * \param len The length you want read
-   * 
+   *
    * \return
    *  -1 -- failure
    *  < len -- eof
    */
-  size_t Read(void* buf, size_t len);
-  
+  size_t Read(void *buf, size_t len);
+
   /**
    * \brief Read a line terminated \r\n(Win) or \n(Linux/Unix)
    * \param need_newline Save the newline characters
@@ -88,9 +89,12 @@ class File : kanon::noncopyable {
    *  E_ERROR -- error
    *  0 -- OK
    */
-  int ReadLine(std::string& line, const bool need_newline = true);
+  int ReadLine(std::string &line, const bool need_newline = true);
 
-  bool Write(void const* buf, size_t len) noexcept { return ::fwrite(buf, 1, len, fp_) < len; }
+  bool Write(void const *buf, size_t len) noexcept
+  {
+    return ::fwrite(buf, 1, len, fp_) < len;
+  }
 
   bool Flush() noexcept { return ::fflush(fp_) != 0; }
   void Rewind() noexcept { ::rewind(fp_); }
@@ -99,21 +103,25 @@ class File : kanon::noncopyable {
   bool SeekEnd(long offset) noexcept { return Seek(offset, SEEK_END) == 0; }
   long GetCurrentPosition() noexcept { return ::ftell(fp_); }
   size_t GetFileSize() const noexcept;
-  
+
   FILE *fp() const noexcept { return fp_; }
   FILE *GetFileHandler() const noexcept { return fp_; }
   bool IsValid() const noexcept { return fp_ != NULL; }
 
   static size_t GetFileSize(char const *path) noexcept;
-  
+
   static const size_t kInvalidReturn = static_cast<size_t>(-1); /* Deprecated */
   static const size_t INVALID_RETURN = static_cast<size_t>(-1);
- private:
-  int Seek(long offset, int whence) noexcept { return ::fseek(fp_, offset, whence); }
 
-  FILE* fp_ = nullptr;
+ private:
+  int Seek(long offset, int whence) noexcept
+  {
+    return ::fseek(fp_, offset, whence);
+  }
+
+  FILE *fp_ = nullptr;
 };
 
-}
+} // namespace util
 
-#endif 
+#endif
